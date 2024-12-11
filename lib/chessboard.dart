e7b4d8a194742a6cba47d7891c97ca4b9a245a34
ayishamajna <ayishamajna3016@gmail.com>
2024-12-11 10:30:08 +0530

    11-12-2024

---
+++ lib/chessboard.dart
@@ -0,0 +1,382 @@
+import 'package:flutter/material.dart';
+
+class Chessboard extends StatefulWidget {
+  const Chessboard({super.key});
+
+  @override
+  State<Chessboard> createState() => _ChessboardState();
+}
+
+class _ChessboardState extends State<Chessboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.white,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
